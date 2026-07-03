7723212505742b4d82068a8b18c94a76e15807fc
Yasmin Moslem <48152713+ymoslem@users.noreply.github.com>
2026-07-03 03:28:21 +0100

    Release v0.1.3

--- pare/__init__.py
+++ pare/__init__.py
@@ -9,7 +9,7 @@ from pare.core.dtype import QuantDtype
 from pare.kv_cache import KVCacheConfig, QuantizedKVCache
 from pare.model.io import load_quantized, save_quantized
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "QuantConfig", "QuantDtype", "quantize",
     "save_quantized", "load_quantized",
